56b10bb704f26b44a7f94f5e4a0e2cfde3796685
Jia <3200884258@qq.com>
2021-01-20 10:02:58 +0800

    1

--- nestjs-mall-server/src/app.module.ts
+++ nestjs-mall-server/src/app.module.ts
@@ -1,7 +1,13 @@
+import { ProductModule } from './modules/product/product.module';
+import { UserModule } from './modules/user/user.module';
+import { OrderModule } from './modules/order/order.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductModule,
+    UserModule,
+    OrderModule,],
   controllers: [],
   providers: [],
 })
